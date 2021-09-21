2ebe35baa66ba84121f39c313f17022e9c50d761
Prasanth Reddy M <prasanthreddy.maddala@gmail.com>
2021-09-21 18:43:27 +0530

    MAUI Essentials - Implementing Audio recording with Plugin.AudioRecorder Open File with and Share File Options implemented for the recorded audio

--- MauiAudioRecorder/MainPage.xaml.cs
+++ MauiAudioRecorder/MainPage.xaml.cs
@@ -1,24 +1,124 @@
 ﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 using Microsoft.Maui.Essentials;
+using Microsoft.Maui.Graphics;
+using Plugin.AudioRecorder;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace MauiAudioRecorder
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private readonly AudioRecorderService audioRecorderService = new AudioRecorderService();
+
+        private readonly AudioPlayer audioPlayer = new AudioPlayer();
+
 
         public MainPage()
         {
             InitializeComponent();
+            RecordButton.BackgroundColor = Color.FromArgb("#561ECC");
+            PlayButton.IsEnabled = false;
+            PlayButton.BackgroundColor = Color.FromArgb("#C8C8C3");
+            ShareButton.IsEnabled = false;
+            ShareButton.BackgroundColor = Color.FromArgb("#C8C8C3");
+
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnRecordClicked(object sender, EventArgs e)
         {
-            count++;
-            CounterLabel.Text = $"Current count: {count}";
+            await ValidatePermissionsAsync().ConfigureAwait(false);
 
-            SemanticScreenReader.Announce(CounterLabel.Text);
+            if (audioRecorderService.IsRecording)
+            {
+                RecordButton.Text = "Start Recording";
+                await audioRecorderService.StopRecording();
+                Progress.Text = "Recording is available";
+                if (!string.IsNullOrEmpty(audioRecorderService.GetAudioFilePath()))
+                {
+                    PlayButton.IsEnabled = true;
+                    PlayButton.BackgroundColor = Color.FromArgb("#561ECC");
+                    ShareButton.IsEnabled = true;
+                    ShareButton.BackgroundColor = Color.FromArgb("#561ECC");
+                }
+                //audioPlayer.Play(audioRecorderService.GetAudioFilePath());
+            }
+            else
+            {
+                audioRecorderService.FilePath = Path.Combine(FileSystem.AppDataDirectory, $"{Guid.NewGuid().ToString().Substring(0, 5)}.wav");
+                await audioRecorderService.StartRecording();
+                RecordButton.Text = "Stop Recording";
+                Progress.Text = "Waiting for recording";
+                PlayButton.IsEnabled = false;
+                PlayButton.BackgroundColor = Color.FromArgb("#C8C8C3");
+                ShareButton.IsEnabled = false;
+                ShareButton.BackgroundColor = Color.FromArgb("#C8C8C3");
+            }
+
+        }
+
+        private async void OnShareButtonClicked(object sender, EventArgs e)
+        {
+            var sourcePath = audioRecorderService.GetAudioFilePath();
+
+            if (string.IsNullOrEmpty(sourcePath))
+                return;
+            
+            await Share.RequestAsync(new ShareFileRequest
+            {
+                Title = "Sharing Recorded Audio File",
+                File = new ShareFile(sourcePath)
+            });
+        }
+
+        private async void OnPlayButtonClicked(object sender, EventArgs e)
+        {
+            var sourcePath = audioRecorderService.GetAudioFilePath();
+
+            if (string.IsNullOrEmpty(sourcePath))
+                return;
+
+            await Launcher.OpenAsync(new OpenFileRequest
+            {
+                File = new ReadOnlyFile(sourcePath)
+            });
+
+        }
+
+
+
+        private async Task ValidatePermissionsAsync()
+        {
+            var status = await Permissions.RequestAsync<Permissions.Microphone>();
+            
+            if (status != PermissionStatus.Granted)
+            {
+                Progress.Text = "Microphone permission not granted";
+            }
+
+            status = await Permissions.RequestAsync<Permissions.StorageRead>();
+
+            if (status != PermissionStatus.Granted)
+            {
+                Progress.Text = "StorageRead permission not granted";
+            }
+
+            status = await Permissions.RequestAsync<Permissions.StorageWrite>();
+
+            if (status != PermissionStatus.Granted)
+            {
+                Progress.Text = "StorageWrite permission not granted";
+            }
+
+            status = await Permissions.RequestAsync<Permissions.Speech>();
+
+            if (status != PermissionStatus.Granted)
+            {
+                Progress.Text = "Speech permission not granted";
+            }
+
         }
     }
 }
--- MauiAudioRecorder/Platforms/Android/MainActivity.cs
+++ MauiAudioRecorder/Platforms/Android/MainActivity.cs
@@ -1,5 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
 using Microsoft.Maui;
 
 namespace MauiAudioRecorder
@@ -7,5 +9,15 @@ namespace MauiAudioRecorder
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : MauiAppCompatActivity
     {
-    }
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			Microsoft.Maui.Essentials.Platform.Init(this, savedInstanceState);
+			base.OnCreate(savedInstanceState);
+		}
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+		{
+			Microsoft.Maui.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
 }
